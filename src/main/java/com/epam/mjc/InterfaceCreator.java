40259d18b82248e010adf5dbb61e47b879553071
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-14 00:17:17 +0500

    Done with lambdas(not stream api)

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,11 +1,20 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        return x -> x.stream().map(e -> e / divider).collect(Collectors.toList());
+        return x -> {
+            List<Integer> data= new ArrayList<>();
+            for (Integer i :
+                    x) {
+                data.add(i/divider);
+            }
+            return data;
+        };
 
     }
 }
